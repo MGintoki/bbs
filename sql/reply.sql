39a624f404d7643b3291630eec7217e1db7bd4a6
MGintoki <904448980@qq.com>
2018-06-19 14:30:42 +0800

    s

---
+++ sql/reply.sql
@@ -0,0 +1,1 @@
+INSERT INTO `reply` VALUES (15, 1, '1L', 'test', '2018-6-8 23:27:51', 0, 0);
---
+++ sql/user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` VALUES (1, 'test', 'qwert', 1, 'modifyMessage', '2018-6-5 00:00:00', 0, 'test.jpg', 0, '2018-6-11 13:50:03');
+INSERT INTO `user` VALUES (2, 'login', '123456', 1, 'test', '2018-6-5 18:49:02', 0, NULL, 0, NULL);
+INSERT INTO `user` VALUES (3, 'testRegister', '123123', 0, 'It\'s a test', '2018-6-7 20:35:52', 0, 'images/userHead/MGintoki.jpg', 0, NULL);
+INSERT INTO `user` VALUES (4, 'MGintoki', '123', 1, 'MGintoki', '2018-6-17 18:04:11', 1, 'MGintoki.jpg', 0, NULL);
